2accc71b7064c218454ab26a87a1dc71503b7afb
hromau <skleroz_by@mail.ru>
2018-01-24 21:08:28 +0300

    regex dont work

---
+++ Kontur.ImageTransformer/AsyncHttpServer.cs
@@ -0,0 +1,231 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+using System.Drawing;
+
+namespace Kontur.ImageTransformer
+{
+    internal class AsyncHttpServer : IDisposable
+    {
+
+        private readonly HttpListener listener;
+
+        private Thread listenerThread;
+        private bool disposed;
+        private volatile bool isRunning;
+
+        public static Regex urlGrayscale = new Regex(@"\w+(/process/grayscale/\d,\d,\d,\d/)");
+        public static Regex urlSepia = new Regex(@"\w+/process/sepia/\d,\d,\d,\d/");
+        public static Regex urlThreshold = new Regex(@"\w+/process/threshold(\d{100})/\d,\d,\d,\d/");
+
+        public AsyncHttpServer()
+        {
+            listener = new HttpListener();
+        }
+        
+        public void Start(string prefix)
+        {
+            lock (listener)
+            {
+                if (!isRunning)
+                {
+                    listener.Prefixes.Clear();
+                    listener.Prefixes.Add(prefix);
+                    listener.Start();
+
+                    listenerThread = new Thread(Listen)
+                    {
+                        IsBackground = true,
+                        Priority = ThreadPriority.Highest
+                    };
+                    listenerThread.Start();
+                    
+                    isRunning = true;
+                }
+            }
+        }
+
+        public void Stop()
+        {
+            lock (listener)
+            {
+                if (!isRunning)
+                    return;
+
+                listener.Stop();
+
+                listenerThread.Abort();
+                listenerThread.Join();
+                
+                isRunning = false;
+            }
+        }
+
+        public void Dispose()
+        {
+            if (disposed)
+                return;
+
+            disposed = true;
+
+            Stop();
+
+            listener.Close();
+        }
+        
+        private void Listen()
+        {
+            while (true)
+            {
+                try
+                {
+                    if (listener.IsListening)
+                    {
+                        var context = listener.GetContext();
+                        Task.Run(() => HandleContextAsync(context));
+                        Console.WriteLine("New Request");
+                    }
+                    else Thread.Sleep(0);
+                }
+                catch (ThreadAbortException)
+                {
+                    return;
+                }
+                catch (Exception error)
+                {
+                    // TODO: log errors
+                }
+            }
+        }
+
+        public static byte CheckUrl(string url,ref byte x, ref byte y, ref byte w, ref byte h,ref byte thresholdX)
+        {
+            char[] sp = { ',', '/', '(', ')' };
+            string[] temp0;
+            if (urlGrayscale.IsMatch(url))
+            {
+                //grayscale
+               temp0 = url.Split(sp);
+                x = byte.Parse( temp0[5]);
+                y = byte.Parse(temp0[6]);
+                w = byte.Parse(temp0[7]);
+                h = byte.Parse(temp0[8]);
+
+                return 1;
+            }
+
+            if (urlSepia.IsMatch(url))
+            {
+                //sepia
+                temp0 = url.Split(sp);
+                x = byte.Parse(temp0[5]);
+                y = byte.Parse(temp0[6]);
+                w = byte.Parse(temp0[7]);
+                h = byte.Parse(temp0[8]);
+                return 2;
+            }
+
+            if (urlThreshold.IsMatch(url))
+            {
+                //threshold
+                temp0 = url.Split(sp);
+                x = byte.Parse(temp0[5]);
+                y = byte.Parse(temp0[6]);
+                w = byte.Parse(temp0[7]);
+                h = byte.Parse(temp0[8]);
+                thresholdX = byte.Parse(temp0[5]);
+                return 3;
+            }
+            x = 0;
+            y = 0;
+            w = 0;
+            h = 0;
+            return 0;
+        }
+
+        public static bool CheckFileSize(long contentLength)
+        {
+            var fileSize = Math.Round(Convert.ToDouble(contentLength) / 1024.0, 2);
+            if (fileSize <=100)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        private async Task HandleContextAsync(HttpListenerContext listenerContext)
+        {
+            // TODO: implement request handling
+            byte x=0;
+            byte y=0;
+            byte w=0;
+            byte h=0;
+            byte thresholdX=0;
+
+            byte methodFilter = CheckUrl(listenerContext.Request.Url.ToString(),ref x,ref y,ref w,ref h,ref thresholdX);
+
+            if (listenerContext.Request.HttpMethod.ToString() == "POST" && methodFilter !=0 &&
+                listenerContext.Request.HasEntityBody == true && CheckFileSize(listenerContext.Request.ContentLength64))
+            {
+                Stream data = listenerContext.Request.InputStream;
+                int asd = data.ReadByte();
+                long contentLength = listenerContext.Request.ContentLength64;
+                byte[] buffer = new byte[contentLength]; //BODY
+                data.Read(buffer, 0, asd);
+
+                byte[] responseBodyArray=null;
+                switch (methodFilter)
+                {
+                    case 1:
+                        {
+                            //grayscale
+                            responseBodyArray = ImageFiltering.GetBytesOutputArray(ImageFiltering.DrawAsGrayscale(ref buffer, x, y, w, h));
+                            break;
+                        }
+                    case 2:
+                        {
+                            //sepia
+                            responseBodyArray = ImageFiltering.GetBytesOutputArray(ImageFiltering.DrawAsSepiaTone(ref buffer, x, y, w, h));
+                            break;
+                        }
+                    case 3:
+                        {
+                            //threshold
+                            break;
+                        }
+                    default:break;
+                }
+
+                //listenerContext.Response.ContentLength64 = outputBodyArray.LongLength;
+                listenerContext.Response.OutputStream.SetLength(responseBodyArray.LongLength);
+                listenerContext.Response.OutputStream.Read(responseBodyArray, 0, responseBodyArray.Length);
+               
+
+            }
+            else
+            {
+                return;
+            }
+            
+            
+            string body2 = listenerContext.Request.QueryString.ToString();
+            string headers = listenerContext.Request.Headers.ToString();
+            
+
+           
+            using (var writer = new StreamWriter(listenerContext.Response.OutputStream))
+            {
+                if (listenerContext.Response.StatusCode == (int)HttpStatusCode.OK)
+                    writer.WriteLine(HttpStatusCode.OK.ToString());
+                //writer.WriteLine("Hello, world!");
+                
+            }
+        }
+
+        
+    }
+}
---
+++ Kontur.ImageTransformer/ImageFiltering.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Runtime.InteropServices;
+
+namespace Kontur.ImageTransformer
+{
+    public static class ImageFiltering
+    {
+        //private static Bitmap GetArgbCopy(this Image sourceImage, ref byte[] byteBuffer, byte x,byte y,byte w,byte h)
+        //{
+        //    //Bitmap bmpNew = new Bitmap(w, h, PixelFormat.Format32bppArgb);
+
+        //    Bitmap bmpNew = MakeBitmap(byteBuffer, w, h);
+
+        //    //Bitmap bitmap = AsBitmap(ref sourceImage, ref byteBuffer, x, y, w, h);
+
+        //    BitmapData bmpData = bmpNew.LockBits(new Rectangle(x, y, w, h), ImageLockMode.ReadOnly, PixelFormat.Format32bppArgb);
+
+        //    IntPtr ptr = bmpData.Scan0;
+
+        //    //обратно
+        //    Marshal.Copy(byteBuffer, 0, ptr, byteBuffer.Length);
+
+        //    bmpNew.UnlockBits(bmpData);
+
+        //    //bmpData = null;
+        //    //byteBuffer = null;
+
+        //    using (Graphics graphics = Graphics.FromImage(bmpNew))
+        //    {
+        //        graphics.DrawImage(sourceImage, new Rectangle(x, y, bmpNew.Width, bmpNew.Height), new Rectangle(x, y, bmpNew.Width, bmpNew.Height), GraphicsUnit.Pixel);
+        //        graphics.Flush();
+        //    }
+
+        //    return bmpNew;
+        //}
+
+        //идет кодирование и обратно
+        //public static Bitmap AsBitmap(ref Image sourceImage,ref byte[] byteBuffer, byte x, byte y, byte w, byte h)
+        //{
+        //    Bitmap bmpNew = GetArgbCopy(ref sourceImage,x,y,w,h);
+        //    BitmapData bmpData = bmpNew.LockBits(new Rectangle(x, y, w, h), ImageLockMode.ReadOnly, PixelFormat.Format32bppArgb);
+
+        //    IntPtr ptr = bmpData.Scan0;
+
+        //    //обратно
+        //    Marshal.Copy(byteBuffer, 0, ptr, byteBuffer.Length);
+
+        //    bmpNew.UnlockBits(bmpData);
+
+        //    bmpData = null;
+        //    byteBuffer = null;
+
+        //    return bmpNew;
+        //}
+
+        public static Bitmap DrawAsGrayscale(ref byte[] byteBuffer, byte x, byte y, byte w, byte h)
+        {
+            ColorMatrix colorMatrix = new ColorMatrix(new float[][]
+                                                {
+                                                    new float[] {.3f, .3f, .3f, 0, 0},
+                                                    new float[] {.59f, .59f, .59f, 0, 0},
+                                                    new float[] {.11f, .11f, .11f, 0, 0},
+                                                    new float[] {0, 0, 0, 1, 0},
+                                                    new float[] {0, 0, 0, 0, 1}
+                                                });
+
+            return ApplyColorMatrix(colorMatrix,ref byteBuffer,x,y,w,h);
+        }
+
+        private static Bitmap ApplyColorMatrix(ColorMatrix colorMatrix,ref byte[] byteBuffer, byte x, byte y, byte w, byte h)
+        {
+            //Bitmap bmp32BppSource = GetArgbCopy(sourceImage, ref byteBuffer,x,y,w,h);
+            Bitmap bmp32BppSource = MakeBitmap(byteBuffer, w, h);
+            Bitmap bmp32BppDest = new Bitmap(bmp32BppSource.Width, bmp32BppSource.Height, PixelFormat.Format32bppArgb);
+
+            using (Graphics graphics = Graphics.FromImage(bmp32BppDest))
+            {
+                ImageAttributes bmpAttributes = new ImageAttributes();
+                bmpAttributes.SetColorMatrix(colorMatrix);
+
+                graphics.DrawImage(bmp32BppSource, new Rectangle(0, 0, bmp32BppSource.Width, bmp32BppSource.Height),
+                                 0, 0, bmp32BppSource.Width, bmp32BppSource.Height, GraphicsUnit.Pixel, bmpAttributes);
+
+            }
+
+            bmp32BppSource.Dispose();
+
+            return bmp32BppDest;
+        }
+
+        public static Bitmap DrawAsSepiaTone(ref byte[] byteBuffer, byte x, byte y, byte w, byte h)
+        {
+            ColorMatrix colorMatrix = new ColorMatrix(new float[][]
+                                                {
+                                                    new float[] {.393f, .349f, .272f, 0, 0},
+                                                    new float[] {.769f, .686f, .534f, 0, 0},
+                                                    new float[] {.189f, .168f, .131f, 0, 0},
+                                                    new float[] {0, 0, 0, 1, 0},
+                                                    new float[] {0, 0, 0, 0, 1}
+                                                });
+
+            return ApplyColorMatrix(colorMatrix,ref byteBuffer, x, y, w, h);
+        }
+
+        //Формируем изображение из массива пикселей
+        public static Bitmap MakeBitmap(byte[] input, byte width, byte height)
+        {
+            if (input.Length % 3 != 0) return null;
+
+            Bitmap output = new Bitmap(width, height);
+            BitmapData image_output_Data = output.LockBits
+                (
+                    new Rectangle(0, 0, width, height),
+                    ImageLockMode.ReadWrite,
+                    PixelFormat.Format32bppArgb
+                );
+
+            System.Runtime.InteropServices.Marshal.Copy
+                (
+                    input,
+                    0,
+                    image_output_Data.Scan0,
+                    input.Length
+                );
+            output.UnlockBits(image_output_Data);
+
+            return output;
+        }
+
+        //Получаем количество байт в загруженном изображении
+        public static byte[] GetBytesOutputArray(Bitmap input_image)
+        {
+            int in_bytes = input_image.Width * input_image.Height * 3;
+            Rectangle rect = new Rectangle
+                (
+                    0,
+                    0,
+                    input_image.Width,
+                    input_image.Height
+                );
+
+            BitmapData input_image_Data = input_image.LockBits
+                (
+                    rect,
+                    ImageLockMode.ReadOnly,
+                    PixelFormat.Format24bppRgb
+                );
+
+
+            byte[] out_bytes = new byte[in_bytes];
+
+            System.Runtime.InteropServices.Marshal.Copy
+                (
+                    input_image_Data.Scan0,
+                    out_bytes,
+                    0,
+                    in_bytes
+                );
+            input_image.UnlockBits(input_image_Data);
+
+            return out_bytes;//На выходе - массив байт
+        }
+
+    }
+}
