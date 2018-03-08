0d9c007bb9d050d480ad9a7d3c063b6f1f7840fe
hromau <skleroz_by@mail.ru>
2018-03-08 23:48:44 +0300

    latest update

--- Kontur.ImageTransformer/AsyncHttpServer.cs
+++ Kontur.ImageTransformer/AsyncHttpServer.cs
@@ -22,9 +22,12 @@ namespace Kontur.ImageTransformer
         private List<Task> listOfTasks = new List<Task>();
         private CancellationTokenSource tokenSource;
 
-        public static Regex urlGrayscale = new Regex(@"\w+/process/grayscale/\d{0,4},\d{0,4},\d{0,4},\d{0,4}/");
-        public static Regex urlSepia = new Regex(@"\w+/process/sepia/\d{0,4},\d{0,4},\d{0,4},\d{0,4}/");
-        public static Regex urlThreshold = new Regex(@"\w+/process/threshold\(\d{0,3}\)/\d{0,4},\d{0,4},\d{0,4},\d{0,4}/");
+
+        public static Regex urlRotateCw = new Regex(@"\w+/process/rotate-cw/\d{0,4},\d{0,4},\d{0,4},\d{0,4}/");
+        public static Regex urlRotateCcw = new Regex(@"\w+/process/rotate-ccw/\d{0,4},\d{0,4},\d{0,4},\d{0,4}/");
+        public static Regex urlFlipV = new Regex(@"\w+/process/flip-v/\d{0,4},\d{0,4},\d{0,4},\d{0,4}/");
+        public static Regex urlFlipH = new Regex(@"\w+/process/flip-h/\d{0,4},\d{0,4},\d{0,4},\d{0,4}/");
+
 
         public AsyncHttpServer()
         {
@@ -125,43 +128,57 @@ namespace Kontur.ImageTransformer
             }
         }
 
-        public static byte CheckUrl(string url,ref byte x, ref byte y, ref byte w, ref byte h,ref byte thresholdX)
+        private static byte CheckUrl(string url,ref byte x, ref byte y, ref byte w, ref byte h)
         {
             char[] sp = { ',', '/', '(', ')' };
-            string[] temp0;
-            if (urlGrayscale.IsMatch(url))
+            string[] temp;
+            if (urlRotateCw.IsMatch(url))
             {
-                //grayscale
-               temp0 = url.Split(sp);
-                x = byte.Parse( temp0[5]);
-                y = byte.Parse(temp0[6]);
-                w = byte.Parse(temp0[7]);
-                h = byte.Parse(temp0[8]);
+                //rotate cw
+
+               temp = url.Split(sp);
+                x = byte.Parse(temp[5]);
+                y = byte.Parse(temp[6]);
+                w = byte.Parse(temp[7]);
+                h = byte.Parse(temp[8]);
 
                 return 1;
             }
 
-            if (urlSepia.IsMatch(url))
+            if (urlRotateCcw.IsMatch(url))
             {
-                //sepia
-                temp0 = url.Split(sp);
-                x = byte.Parse(temp0[5]);
-                y = byte.Parse(temp0[6]);
-                w = byte.Parse(temp0[7]);
-                h = byte.Parse(temp0[8]);
+                //rotate ccw
+
+                temp = url.Split(sp);
+                x = byte.Parse(temp[5]);
+                y = byte.Parse(temp[6]);
+                w = byte.Parse(temp[7]);
+                h = byte.Parse(temp[8]);
                 return 2;
             }
 
-            if (urlThreshold.IsMatch(url))
+            if (urlFlipV.IsMatch(url))
             {
-                //threshold
-                temp0 = url.Split(sp);
-                x = byte.Parse(temp0[7]);
-                y = byte.Parse(temp0[8]);
-                w = byte.Parse(temp0[9]);
-                h = byte.Parse(temp0[10]);
-                thresholdX = byte.Parse(temp0[5]);
+                //flipV
+
+                temp = url.Split(sp);
+                x = byte.Parse(temp[5]);
+                y = byte.Parse(temp[6]);
+                w = byte.Parse(temp[7]);
+                h = byte.Parse(temp[8]);
                 return 3;
+            }
+
+            if (urlFlipH.IsMatch(url))
+            {
+                //FlipH
+
+                temp = url.Split(sp);
+                x = byte.Parse(temp[5]);
+                y = byte.Parse(temp[6]);
+                w = byte.Parse(temp[7]);
+                h = byte.Parse(temp[8]);
+                return 4;
             }
             x = 0;
             y = 0;
@@ -170,7 +187,7 @@ namespace Kontur.ImageTransformer
             return 0;
         }
 
-        public static bool CheckFileSize(long contentLength)
+        private static bool CheckFileSize(long contentLength)
         {
             var fileSize = Math.Round(Convert.ToDouble(contentLength) / 1024.0, 2);
             if (fileSize <=100)
@@ -189,23 +206,27 @@ namespace Kontur.ImageTransformer
             byte y = 0;
             byte w = 0;
             byte h = 0;
-            byte thresholdX = 0;
 
-            byte methodFilter = CheckUrl(listenerContext.Request.Url.ToString(), ref x, ref y, ref w, ref h, ref thresholdX);
+            byte methodFilter = CheckUrl(listenerContext.Request.Url.ToString(), ref x, ref y, ref w, ref h);
 
-            if (w <= 0 || h <= 0)
-            {
-                listenerContext.Response.StatusCode = (int)HttpStatusCode.NoContent;
-                listenerContext.Response.StatusDescription = HttpStatusCode.NoContent.ToString();
-                listenerContext.Response.Close();
-                return;
-            }
+          
 
             if (listenerContext.Request.HttpMethod == "POST" && methodFilter != 0 &&
                 listenerContext.Request.HasEntityBody == true && CheckFileSize(listenerContext.Request.ContentLength64))
             {
                 try
                 {
+                    if (w <= 0 || h <= 0)
+                    {
+                        listenerContext.Response.StatusCode = (int)HttpStatusCode.NoContent;
+                        listenerContext.Response.StatusDescription = HttpStatusCode.NoContent.ToString();
+                        listenerContext.Response.Close();
+
+                        listener.Stop();
+
+                        return;
+                    }
+
                     Stream data = listenerContext.Request.InputStream;
                     long contentLength = listenerContext.Request.ContentLength64;
                     byte[] buffer = new byte[contentLength]; //BODY
@@ -216,14 +237,10 @@ namespace Kontur.ImageTransformer
                     byte[] responseBodyArray = null;
                     switch (methodFilter)
                     {
-                        case 1:
+                        case 1://rotate cw
                             {
-                                //grayscale
-                                responseBodyArray = NewFilter.GetByteOutputArray(NewFilter.DrawAsGrayscale(buffer, x, y, w, h));
-                                //Bitmap temp = ImageFiltering.MakeBitmap(buffer, w, h);
-                                //Bitmap resultFilters = Filter2.DrawAsGrayscale(buffer);
-                                //Image a = Filter2.ByteArraToImage(buffer);
-                                //responseBodyArray = ImageFiltering.GetBytesOutputArray(resultFilters);
+                                responseBodyArray = ModificationImage.RotateCW(buffer, x, y, w, h);
+                             
 
                                 listenerContext.Response.ContentType = "text/html";
                                 if (responseBodyArray == null || responseBodyArray.Length == 0)
@@ -233,28 +250,17 @@ namespace Kontur.ImageTransformer
                                     listenerContext.Response.StatusDescription = HttpStatusCode.BadRequest.ToString();
                                     //listenerContext.Response.OutputStream.SetLength(responseBodyArray.LongLength);
                                 }
-                                //try
-                                //{
-                                //    listenerContext.Response.OutputStream.SetLength(responseBodyArray.LongLength);
-                                //}
-                                //catch(Exception ex)
-                                //{
-                                //    Console.WriteLine(ex.Message);
-                                //}
+                        
                                 listenerContext.Response.ContentLength64 = responseBodyArray.LongLength;
                                 listenerContext.Response.OutputStream.Write(responseBodyArray, 0, responseBodyArray.Length);
 
                                 break;
                             }
-                        case 2:
+                        case 2: //rotate ccw
                             {
-                                //sepia
-                                //Bitmap resultFilters = Filter2.DrawAsSepiaTone(listenerContext.Request.InputStream);
-                                //Image image = Filter2.ByteArraToImage(buffer);
-                                //Bitmap a = Filter2.DrawAsSepiaTone(image);
-                                responseBodyArray = NewFilter.GetByteOutputArray(NewFilter.DrawAsSepiaTone(buffer, x, y, w, h));
+                                responseBodyArray = ModificationImage.RotateCWW(buffer, x, y, w, h);
 
-                                //responseBodyArray = ImageFiltering.GetBytesOutputArray(ImageFiltering.DrawAsSepiaTone(ref buffer, x, y, w, h));
+
 
                                 listenerContext.Response.ContentType = "text/html";
                                 if (responseBodyArray == null)
@@ -262,21 +268,19 @@ namespace Kontur.ImageTransformer
                                     listenerContext.Response.StatusCode = (int)HttpStatusCode.BadRequest;
                                     listenerContext.Response.ContentType = "text/html";
                                     listenerContext.Response.StatusDescription = HttpStatusCode.BadRequest.ToString();
-                                    //listenerContext.Response.OutputStream.SetLength(responseBodyArray.LongLength);
+           
                                 }
                                 listenerContext.Response.ContentEncoding = listenerContext.Request.ContentEncoding;
                                 listenerContext.Response.ContentLength64 = responseBodyArray.LongLength;
-                                //listenerContext.Response.OutputStream.SetLength(responseBodyArray.LongLength);
+            
                                 listenerContext.Response.OutputStream.Write(responseBodyArray, 0, responseBodyArray.Length);
-                                //listener.Stop();
+                  
 
                                 break;
                             }
-                        case 3:
+                        case 3://flipV
                             {
-                                //threshold
-                                //responseBodyArray = NewFilter.DrawAsThreshold(buffer, thresholdX, x, y, w, h);
-                                responseBodyArray = NewFilter.GetByteOutputArray(NewFilter.DrawAsThreshold(buffer,thresholdX,x,y,w,h));
+                                responseBodyArray = ModificationImage.FlipV(buffer, x, y, w, h);
 
                                 if (responseBodyArray == null)
                                 {
@@ -291,12 +295,30 @@ namespace Kontur.ImageTransformer
 
                                 break;
                             }
+                        case 4: //flipH
+                            {
+                                responseBodyArray = ModificationImage.FlipH(buffer, x, y, w, h);
+
+                                if (responseBodyArray == null)
+                                {
+                                    listenerContext.Response.StatusCode = (int)HttpStatusCode.BadRequest;
+                                    listenerContext.Response.ContentType = "text/html";
+                                    listenerContext.Response.StatusDescription = HttpStatusCode.BadRequest.ToString();
+                                    //listenerContext.Response.OutputStream.SetLength(responseBodyArray.LongLength);
+                                }
+                                listenerContext.Response.ContentEncoding = listenerContext.Request.ContentEncoding;
+                                listenerContext.Response.ContentLength64 = responseBodyArray.LongLength;
+                                listenerContext.Response.OutputStream.Write(responseBodyArray, 0, responseBodyArray.Length);
+
+
+                                break;
+                            }
                         default: break;
                     }
 
-                    //listenerContext.Response.ContentLength64 = outputBodyArray.LongLength;
-                    //listenerContext.Response.OutputStream.SetLength(responseBodyArray.LongLength);
-                    //listenerContext.Response.OutputStream.Read(responseBodyArray, 0, responseBodyArray.Length);
+
+                    listener.Stop();
+
 
                 }
                 catch (OperationCanceledException)
@@ -304,20 +326,20 @@ namespace Kontur.ImageTransformer
                     listenerContext.Response.StatusCode = (int)HttpStatusCode.RequestTimeout;
                     listenerContext.Response.ContentType = "text/html";
                     listenerContext.Response.StatusDescription = HttpStatusCode.RequestTimeout.ToString();
+
+                    listener.Stop();
                 }
             }
             else
             {
-
-
                 listenerContext.Response.StatusCode = (int)HttpStatusCode.BadRequest;
                 listenerContext.Response.ContentType = "text/html";
                 listenerContext.Response.StatusDescription = HttpStatusCode.BadRequest.ToString();
                 byte[] a = new byte[listenerContext.Response.StatusDescription.ToString().Length];
                 byte[] toBytes = Encoding.ASCII.GetBytes(listenerContext.Response.StatusDescription.ToString());
-                //listenerContext.Request.InputStream.Read(a, 0, a.Length);
                 listenerContext.Response.OutputStream.Write(toBytes, 0, toBytes.Length);
 
+                listener.Stop();
 
                 return;
             }
---
+++ Kontur.ImageTransformer/ModificationImage.cs
@@ -0,0 +1,239 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kontur.ImageTransformer
+{
+    public class ModificationImage
+    {
+        private static Bitmap CropPhoto(Image sourceImage, byte x, byte y, byte w, byte h)
+        {
+            if (x < 0)
+            {
+                w -= x;
+                x = 0;
+            }
+            if (y < 0)
+            {
+                h -= y;
+                y = 0;
+            }
+            Bitmap bmpNew = new Bitmap(w, h, PixelFormat.Format32bppArgb);
+            using (Graphics graphics = Graphics.FromImage(bmpNew))
+            {
+                graphics.DrawImage(sourceImage, new Rectangle(x, y, bmpNew.Width, bmpNew.Height), new Rectangle(x, y, bmpNew.Width, bmpNew.Height), GraphicsUnit.Pixel);
+                graphics.Flush();
+            }
+            return bmpNew;
+        }
+
+
+        private static Image ArrayToImage(byte[] inputArray)
+        {
+            MemoryStream ms = new MemoryStream(inputArray, 0, inputArray.Length);
+            ms.Write(inputArray, 0, inputArray.Length);
+            try
+            {
+                Image a = Image.FromStream(ms);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message + " " + DateTime.Now);
+                Console.WriteLine(ex.StackTrace);
+            }
+            return Image.FromStream(ms);
+        }
+
+
+        public static byte[] FlipV(byte[] inputArray, byte x,byte y,byte w,byte h)
+        {
+
+            Image sourceImage = ArrayToImage(inputArray);
+
+            Bitmap outputBitmap = CropPhoto(sourceImage, x, y, w, h);
+
+            if (outputBitmap == null)
+            {
+                return null;
+            }
+
+               
+            outputBitmap.RotateFlip(RotateFlipType.RotateNoneFlipY);
+
+
+            int inputBytes = outputBitmap.Width * outputBitmap.Height * 3;
+            Rectangle rect = new Rectangle
+                (
+                    0,
+                    0,
+                    outputBitmap.Width,
+                    outputBitmap.Height
+                );
+
+            BitmapData inputImageData = outputBitmap.LockBits
+                (
+                    rect,
+                    ImageLockMode.ReadOnly,
+                    PixelFormat.Format24bppRgb
+                );
+
+
+            byte[] outputBytes = new byte[inputBytes];
+
+            System.Runtime.InteropServices.Marshal.Copy
+                (
+                    inputImageData.Scan0,
+                    outputBytes,
+                    0,
+                    inputBytes
+                );
+            outputBitmap.UnlockBits(inputImageData);
+
+            return outputBytes;//На выходе - массив байт
+
+        }
+
+
+        public static byte[] FlipH(byte[] inputArray, byte x, byte y, byte w, byte h)
+        {
+            Image sourceImage = ArrayToImage(inputArray);
+
+            Bitmap outputBitmap = CropPhoto(sourceImage, x, y, w, h);
+
+            if (outputBitmap == null)
+            {
+                return null;
+            }
+
+ outputBitmap.RotateFlip(RotateFlipType.RotateNoneFlipX);
+
+            int inputBytes = outputBitmap.Width * outputBitmap.Height * 3;
+            Rectangle rect = new Rectangle
+                (
+                    0,
+                    0,
+                    outputBitmap.Width,
+                    outputBitmap.Height
+                );
+
+            BitmapData inputImageData = outputBitmap.LockBits
+                (
+                    rect,
+                    ImageLockMode.ReadOnly,
+                    PixelFormat.Format24bppRgb
+                );
+
+
+            byte[] outputBytes = new byte[inputBytes];
+
+            System.Runtime.InteropServices.Marshal.Copy
+                (
+                    inputImageData.Scan0,
+                    outputBytes,
+                    0,
+                    inputBytes
+                );
+            outputBitmap.UnlockBits(inputImageData);
+
+            return outputBytes;//На выходе - массив байт
+        }
+
+
+        public static byte[] RotateCW(byte[] inputArray, byte x, byte y, byte w, byte h)
+        {
+            Image sourceImage = ArrayToImage(inputArray);
+
+            Bitmap outputBitmap = CropPhoto(sourceImage, x, y, w, h);
+
+            if (outputBitmap == null)
+            {
+                return null;
+            }
+
+outputBitmap.RotateFlip(RotateFlipType.Rotate90FlipNone);
+
+
+            int inputBytes = outputBitmap.Width * outputBitmap.Height * 3;
+            Rectangle rect = new Rectangle
+                (
+                    0,
+                    0,
+                    outputBitmap.Width,
+                    outputBitmap.Height
+                );
+
+            BitmapData inputImageData = outputBitmap.LockBits
+                (
+                    rect,
+                    ImageLockMode.ReadOnly,
+                    PixelFormat.Format24bppRgb
+                );
+
+
+            byte[] outputBytes = new byte[inputBytes];
+
+            System.Runtime.InteropServices.Marshal.Copy
+                (
+                    inputImageData.Scan0,
+                    outputBytes,
+                    0,
+                    inputBytes
+                );
+            outputBitmap.UnlockBits(inputImageData);
+
+            return outputBytes;//На выходе - массив байт
+        }
+
+
+        public static byte[] RotateCWW(byte [] inputArray, byte x, byte y, byte w, byte h)
+        {
+            Image sourceImage = ArrayToImage(inputArray);
+
+            Bitmap outputBitmap = CropPhoto(sourceImage, x, y, w, h);
+
+            if (outputBitmap == null)
+            {
+                return null;
+            }
+
+ outputBitmap.RotateFlip(RotateFlipType.Rotate270FlipNone);
+
+            int inputBytes = outputBitmap.Width * outputBitmap.Height * 3;
+            Rectangle rect = new Rectangle
+                (
+                    0,
+                    0,
+                    outputBitmap.Width,
+                    outputBitmap.Height
+                );
+
+            BitmapData inputImageData = outputBitmap.LockBits
+                (
+                    rect,
+                    ImageLockMode.ReadOnly,
+                    PixelFormat.Format24bppRgb
+                );
+
+
+            byte[] outputBytes = new byte[inputBytes];
+
+            System.Runtime.InteropServices.Marshal.Copy
+                (
+                    inputImageData.Scan0,
+                    outputBytes,
+                    0,
+                    inputBytes
+                );
+            outputBitmap.UnlockBits(inputImageData);
+
+            return outputBytes;//На выходе - массив байт
+      
+        }
+
+    }
+}
